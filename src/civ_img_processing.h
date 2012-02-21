6d74d4bd820e7be2ef218fa9c8e5e6e1024bbce3
Sho Nakatani <lay.sakura@gmail.com>
2012-02-21 10:29:00 +0900

    Forgot to add files
    Signed-off-by: Sho Nakatani <lay.sakura@gmail.com>

---
+++ src/civ_img_processing.h
@@ -0,0 +1,11 @@
+#ifndef _CIV_COLOR_SUBTRACTION_H_
+#define _CIV_COLOR_SUBTRACTION_H_
+
+
+void
+color_subtraction_by_RGB_distance(IplImage *img,
+                                  const civ_RGB *palette,
+                                  const int palette_len);
+
+
+#endif /* _CIV_COLOR_SUBTRACTION_H_ */
